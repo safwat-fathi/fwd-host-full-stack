d2328b850e677625e0bdb71625df8602f5cb985b
Safwat Fathi <safwat.rashwan@gmail.com>
2022-12-16 10:44:33 +0200

    update: env vars for DB

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,35 +3,40 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const { RDS_DB, RDS_HOST, RDS_USER, RDS_PASSWORD } = (process.env as {
-  RDS_DB: string;
-  RDS_HOST: string;
-  // RDS_PORT: number;
-  RDS_USER: string;
-  RDS_PASSWORD: string;
-}) || {
-  RDS_DB: "",
-  RDS_HOST: "",
-  // RDS_PORT: "",
-  RDS_USER: "",
-  RDS_PASSWORD: "",
-};
+const {
+  ENV,
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB_DEV,
+  POSTGRES_HOST_DEV,
+  POSTGRES_USER_DEV,
+  POSTGRES_PASSWORD_DEV,
+} = <
+  {
+    ENV: string;
+    POSTGRES_DB: string;
+    POSTGRES_HOST: string;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DB_DEV: string;
+    POSTGRES_HOST_DEV: string;
+    POSTGRES_USER_DEV: string;
+    POSTGRES_PASSWORD_DEV: string;
+  }
+>process.env || { ENV: "", POSTGRES_DB: "", POSTGRES_HOST: "", POSTGRES_USER: "", POSTGRES_PASSWORD: "", POSTGRES_DB_DEV: "", POSTGRES_HOST_DEV: "", POSTGRES_USER_DEV: "", POSTGRES_PASSWORD_DEV: "" };
 
 const Client = new Pool({
-  host: RDS_HOST,
+  host: ENV === "dev" ? POSTGRES_HOST_DEV : POSTGRES_HOST,
   port: 5432,
-  database: RDS_DB,
-  user: RDS_USER,
-  password: RDS_PASSWORD,
+  database: ENV === "dev" ? POSTGRES_DB_DEV : POSTGRES_DB,
+  user: ENV === "dev" ? POSTGRES_USER_DEV : POSTGRES_USER,
+  password: ENV === "dev" ? POSTGRES_PASSWORD_DEV : POSTGRES_PASSWORD,
 });
 
-Client.on("connect", () => {
+Client.once("connect", () => {
   console.log("Connected to database.");
-});
-
-Client.on("error", () => {
-  console.log("Error connecting to database.");
-  throw new Error(`Error closing DB connection`);
 });
 
 export default Client;
--- server/src/types/env.d.ts
+++ server/src/types/env.d.ts
@@ -1,12 +1,16 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      ENV: string;
       PORT: string;
-      RDS_HOST: string;
-      RDS_PORT: number;
-      RDS_DB: string;
-      RDS_USER: string;
-      RDS_PASSWORD: string;
+      POSTGRES_HOST_DEV: string;
+      POSTGRES_DB_DEV: string;
+      POSTGRES_USER_DEV: string;
+      POSTGRES_PASSWORD_DEV: string;
+      POSTGRES_HOST: string;
+      POSTGRES_DB: string;
+      POSTGRES_USER: string;
+      POSTGRES_PASSWORD: string;
       BCRYPT_PASSWORD: string;
       SALT_ROUNDS: string;
       JWT_SECRET: string;
